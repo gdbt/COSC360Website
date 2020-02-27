3b1745604f7f911f0d534d3d0ef41a6070147508
gdbt <gdbt@hotmail.ca>
2020-02-27 14:34:10 -0800

    created php folder and moved required files

--- php/connection.php
+++ php/connection.php
@@ -2,7 +2,6 @@
 	$servername = "138.197.139.183"; //138.197.139.183
 	$username = "main";
 	$password = "P\$YCHO3367Lame";
-
 	try{
 		$pdo = new PDO("mysql:host=$servername;dbname=cosc_db",$username, $password);
 
